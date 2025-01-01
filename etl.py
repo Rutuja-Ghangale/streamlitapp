fd2fd449a9ec3accf6a393b739814fdca10488ed
rutujadg05@gmail.com <rutujadg05@gmail.com>
2025-01-01 15:20:47 +0530

    file added

--- etl.py
+++ etl.py
@@ -1,11 +1,12 @@
 # I want to show task & status o/p in table format
 
+import pandas as pd
 import streamlit as st
-import pandas as pd 
+
+st.write('AVD')
 data = {
         'Task': ['Extract', 'Transfer', 'Load'],
          'Status': ['Completed','Inprogress','Pending']
     }
 df = pd.DataFrame(data)
-st.write('ETL pipline execution status1')
-st.write(df)
+st.write('ETL pipline execution status1:', df)
